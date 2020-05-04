f821ca3b4a14d4fb90cf0e8c8b5466c8558ea6eb
teju <teju@teju.com>
2020-05-05 01:29:21 +0530

    Fixed all Layouts- before attempting to add TableCarousel

--- src/Demo/Covid/Default.js
+++ src/Demo/Covid/Default.js
@@ -5,115 +5,148 @@ import Aux from "../../hoc/_Aux";
 import DEMO from "../../store/constant";
 import MyCarousel from "../MyCarousel";
 
+import BootstrapTable from '../Tables/BootstrapTable';
+import TableCarousel from '../MyCarousel/TableCarousel'
+
 import avatar1 from '../../assets/images/user/avatar-1.jpg';
 import avatar2 from '../../assets/images/user/avatar-2.jpg';
 import avatar3 from '../../assets/images/user/avatar-3.jpg';
 import "semantic-ui-css/semantic.min.css";
 import "react-multi-carousel/lib/styles.css";
 import '../../assets/styles.css'
-import BootstrapTable from '../Tables/BootstrapTable';
 
 class Covid extends React.Component {
     render() {
         return (
             <Aux>
                 <Row>
-                    <Col md={2}>
-                        <Card>
-                            <Card.Body>
-                                <h6 className='mb-4 '>Total Infected</h6>
-                                <div className="row d-flex align-items-center">
-                                    <div className="col-9">
-                                        <h3 className="f-w-300 d-flex align-items-center m-b-0">
-                                            <i className="feather text-c-red f-30 m-r-5"/>2,456,777</h3>
-                                    </div>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={2}>
-                        <Card>
-                            <Card.Body>
-                                <h6 className='mb-4 '>Total Deceased</h6>
-                                <div className="row d-flex align-items-center">
-                                    <div className="col-9">
-                                        <h3 className="f-w-300 d-flex align-items-center m-b-0">
-                                            <i className="feather text-c-red f-30 m-r-5"/>2,456,777</h3>
-                                    </div>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={2}>
-                        <Card>
-                            <Card.Body>
-                                <h6 className='mb-4 '>Total Recovered</h6>
-                                <div className="row d-flex align-items-center">
-                                    <div className="col-9">
-                                        <h3 className="f-w-300 d-flex align-items-center m-b-0">
-                                            <i className="feather text-c-red f-30 m-r-5"/>2,456,777</h3>
-                                    </div>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={2}>
-                        <Card>
-                            <Card.Body>
-                                <h6 className='mb-4 '>Total Infected</h6>
-                                <div className="row d-flex align-items-center">
-                                    <div className="col-9">
-                                        <h3 className="f-w-300 d-flex align-items-center m-b-0">
-                                            <i className="feather text-c-red f-30 m-r-5"/>2,456,777</h3>
-                                    </div>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={2}>
-                        <Card>
-                            <Card.Body>
-                                <h6 className='mb-4 '>Total Deceased</h6>
-                                <div className="row d-flex align-items-center">
-                                    <div className="col-9">
-                                        <h3 className="f-w-300 d-flex align-items-center m-b-0">
-                                            <i className="feather text-c-red f-30 m-r-5"/>2,456,777</h3>
-                                    </div>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={2}>
-                        <Card>
-                            <Card.Body>
-                                <h6 className='mb-4 '>Total Recovered</h6>
-                                <div className="row d-flex align-items-center">
-                                    <div className="col-9">
-                                        <h3 className="f-w-300 d-flex align-items-center m-b-0">
-                                            <i className="feather text-c-red f-30 m-r-5"/>2,456,777</h3>
-                                    </div>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    
-
-                </Row>
-                <Row>
                     <Col md={6}>
+                        <Row>
+                            <Col md={4}>
+                                <Card>
+                                    <Card.Body>
+                                        <h6 className='mb-4 '>Total Infected</h6>
+                                        <div className="row d-flex align-items-center">
+                                            <div className="col-9">
+                                                <h3 className="f-w-300 d-flex align-items-center m-b-0">
+                                                    <i className="feather text-c-red f-30 m-r-5"/>2,456,777</h3>
+                                            </div>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col md={4}>
+                                <Card>
+                                    <Card.Body>
+                                        <h6 className='mb-4 '>Total Deceased</h6>
+                                        <div className="row d-flex align-items-center">
+                                            <div className="col-9">
+                                                <h3 className="f-w-300 d-flex align-items-center m-b-0">
+                                                    <i className="feather text-c-red f-30 m-r-5"/>2,456,777</h3>
+                                            </div>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col md={4}>
+                                <Card>
+                                    <Card.Body>
+                                        <h6 className='mb-4 '>Total Deceased</h6>
+                                        <div className="row d-flex align-items-center">
+                                            <div className="col-9">
+                                                <h3 className="f-w-300 d-flex align-items-center m-b-0">
+                                                    <i className="feather text-c-red f-30 m-r-5"/>2,456,777</h3>
+                                            </div>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        
+                        </Row>
                         <MyCarousel></MyCarousel>
+                            
+                        <Row>
+                            <Col md={6}>
+                                <Card>
+                                    <Card.Body>
+                                        <h6 className='mb-4 '>Total Infected</h6>
+                                        <div className="row d-flex align-items-center">
+                                            <div className="col-9">
+                                                <h3 className="f-w-300 d-flex align-items-center m-b-0">
+                                                    <i className="feather text-c-red f-30 m-r-5"/>2,456,777</h3>
+                                            </div>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col md={6}>
+                                <Card>
+                                    <Card.Body>
+                                        <h6 className='mb-4 '>Total Deceased</h6>
+                                        <div className="row d-flex align-items-center">
+                                            <div className="col-9">
+                                                <h3 className="f-w-300 d-flex align-items-center m-b-0">
+                                                    <i className="feather text-c-red f-30 m-r-5"/>2,456,777</h3>
+                                            </div>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md={4}>
+                                <Card>
+                                    <Card.Body>
+                                        <h6 className='mb-4 '>Total Infected</h6>
+                                        <div className="row d-flex align-items-center">
+                                            <div className="col-9">
+                                                <h3 className="f-w-300 d-flex align-items-center m-b-0">
+                                                    <i className="feather text-c-red f-30 m-r-5"/>2,456,777</h3>
+                                            </div>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col md={4}>
+                                <Card>
+                                    <Card.Body>
+                                        <h6 className='mb-4 '>Total Deceased</h6>
+                                        <div className="row d-flex align-items-center">
+                                            <div className="col-9">
+                                                <h3 className="f-w-300 d-flex align-items-center m-b-0">
+                                                    <i className="feather text-c-red f-30 m-r-5"/>2,456,777</h3>
+                                            </div>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col md={4}>
+                        <Card>
+                            <Card.Body>
+                                <h6 className='mb-4 '>Total Recovered</h6>
+                                <div className="row d-flex align-items-center">
+                                    <div className="col-9">
+                                        <h3 className="f-w-300 d-flex align-items-center m-b-0">
+                                            <i className="feather text-c-red f-30 m-r-5"/>2,456,777</h3>
+                                    </div>
+                                </div>
+                            </Card.Body>
+                        </Card>
                     </Col>
-                
-                    <Col md={2} >
-                            <BootstrapTable>    </BootstrapTable>
+
+                        </Row>
                     </Col>
                     <Col md={2} >
-                            <BootstrapTable>    </BootstrapTable>
+                        <BootstrapTable>    </BootstrapTable>
                     </Col>
                     <Col md={2} >
-                            <BootstrapTable>    </BootstrapTable>
+                        <BootstrapTable>    </BootstrapTable>
+                    </Col>
+                    <Col md={2} >
+                        <BootstrapTable>    </BootstrapTable>
                     </Col>
                 </Row>
+                
             </Aux>
         );
     }
---
+++ src/Demo/MyCarousel/TableCarousel.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import Carousel from "react-multi-carousel";
+import { Image } from "semantic-ui-react";
+
+import BootstrapTable from '../Tables/BootstrapTable';
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+    paritialVisibilityGutter: 20
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    paritialVisibilityGutter: 50
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    paritialVisibilityGutter: 30
+  }
+};
+
+// Because this is an inframe, so the SSR mode doesn't not do well here.
+// It will work on real devices.
+class TableCarousel extends React.Component{
+
+      
+  intervalID;
+  state = {
+    rand: "100",
+  }
+
+  componentDidMount() {
+    //this.getData();
+
+    /*
+      Now we need to make it run at a specified interval,
+      bind the getData() call to `this`, and keep a reference
+      to the invterval so we can clear it later.
+    */
+   //this.intervalID = setInterval(this.getData.bind(this), 5000);
+  }
+
+  componentWillUnmount() {
+    /*
+      stop getData() from continuing to run even
+      after unmounting this component. Notice we are calling
+      'clearTimeout()` here rather than `clearInterval()` as
+      in the previous example.
+    */
+    //clearInterval(this.intervalID);
+  }
+
+  getData = () => {
+    this.setState({
+        rand : Math.random( 100000)
+    });
+}
+
+render() {
+  return (
+    <Carousel
+    swipeable={false}
+    draggable={false}
+    showDots={true}
+    arrows = {false}
+    //autoPlay
+    //autoPlaySpeed={20000}
+    infinite
+    customTransition="transform 300ms ease-in-out"
+    ssr
+    partialVisbile
+    deviceType="desktop"
+    itemClass="image-item"
+    responsive={responsive}
+      
+    >
+        <BootstrapTable></BootstrapTable> 
+        <BootstrapTable></BootstrapTable> 
+        <BootstrapTable></BootstrapTable> 
+        <BootstrapTable></BootstrapTable>      
+    </Carousel>
+  );
+}
+};
+
+export default TableCarousel;
